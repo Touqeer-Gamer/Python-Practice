b5121724886fae29f7ddabbddb3d1538724ac669
Touqeer Ahmad <touqu09@gmail.com>
2026-07-08 16:08:10 +0500

    new file

---
+++ conditional_statements.py
@@ -0,0 +1,8 @@
+age =  int(input("Your Age : " ))
+
+if age >= 18:
+    print("You Are Eligible For The CNIC")
+
+else :
+    print("you Are Not Elgigble for The Cnic")
+ 
---
+++ username_password_checker.py
@@ -0,0 +1,10 @@
+correct_username = "Touqu007"
+correct_password = "Password00"
+
+user_name = input("Enter Your User Name Here : ")
+pass_word = input("Enter Your Password Here : ")
+
+if user_name == correct_username and pass_word == correct_password :
+    print("     ===.Logged In Sucessfully.===     ")
+else :
+    print("***.User Name OR Password Is Incorrect.***")
