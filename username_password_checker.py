c79048d7f26d778b9837d5666b023a5165829c1a
Touqeer Ahmad <touqu09@gmail.com>
2026-07-09 13:56:07 +0500

    made somechanges

--- username_password_checker.py
+++ username_password_checker.py
@@ -1,10 +1,17 @@
 correct_username = "Touqu007"
 correct_password = "Password00"
 
-user_name = input("Enter Your User Name Here : ")
-pass_word = input("Enter Your Password Here : ")
+max_attempt = 3
+attempt = 0
 
-if user_name == correct_username and pass_word == correct_password :
-    print("     ===.Logged In Sucessfully.===     ")
-else :
-    print("***.User Name OR Password Is Incorrect.***")
+while attempt < 3:   
+    user_name = input("Enter Your User Name Here : ")
+    pass_word = input("Enter Your Password Here : ")
+    attempt += 1
+
+    if user_name == correct_username and pass_word == correct_password :
+        print("     ===.Logged In Sucessfully.===     ")
+        break   
+    else :
+        print("***.User Name OR Password Is Incorrect.***")
+        print(f"You have {max_attempt - attempt} More Attempts Reamaining ")
